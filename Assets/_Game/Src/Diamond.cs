2863331b0a045400af4a1ef60568070267945f09
Sacristan <girts.kesteris@gmail.com>
2020-12-12 18:59:30 +0200

    initial commit

---
+++ Assets/_Game/Src/Diamond.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    [SerializeField] float animationSpeed = 0.5f;
+    [SerializeField] float movementInterval = 0.3f;
+
+    float originalY;
+
+    private void Awake()
+    {
+        originalY = transform.position.y;
+    }
+
+    private void Update()
+    {
+        float y = Mathf.PingPong(Time.time * animationSpeed, movementInterval);
+
+        transform.localPosition = new Vector3(
+            transform.localPosition.x,
+            originalY + y,
+            transform.localPosition.z
+        );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) GameManager.Collect(this);
+    }
+
+}
---
+++ Assets/_Game/Src/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+
+    List<Diamond> uncollectedDiamonds = new List<Diamond>();
+
+    [SerializeField] AudioClip collectSFX;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        uncollectedDiamonds = new List<Diamond>(FindObjectsOfType<Diamond>());
+    }
+
+    public static void Collect(Diamond diamond)
+    {
+        AudioSource.PlayClipAtPoint(instance.collectSFX, diamond.transform.position);
+
+        instance.uncollectedDiamonds.Remove(diamond);
+        Destroy(diamond.gameObject);
+
+        if (instance.uncollectedDiamonds.Count <= 0)
+        {
+            instance.GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GameOver");
+
+        instance.StartCoroutine(RestartGame());
+    }
+
+    IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(0);
+    }
+
+
+}
